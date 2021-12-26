951d16b8ad6314366bf88dfedc1e7b3678da1aea
cov <hsco.dev@gmail.com>
2021-12-27 05:24:17 +0900

    좀 더 단순한 padding 설정 CustomTabLayout 구현

--- app/src/main/java/dev/hsco/oops/presentation/main/MainFragment.kt
+++ app/src/main/java/dev/hsco/oops/presentation/main/MainFragment.kt
@@ -5,9 +5,10 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import com.google.android.material.tabs.TabLayoutMediator
 import dev.hsco.oops.databinding.FragmentMainBinding
 import dev.hsco.oops.databinding.FragmentMainBindingImpl
-import dev.hsco.oops.presentation.util.custom_view.FixedTabLayoutMediator
+import dev.hsco.oops.databinding.ItemMainBottomTabBinding
 
 
 class MainFragment : Fragment() {
@@ -28,7 +29,15 @@ class MainFragment : Fragment() {
     private fun initView() {
         val mainPagerAdapter = MainPagerAdapter(this)
         binding.viewPager.adapter = mainPagerAdapter
-        FixedTabLayoutMediator(binding.viewPager, binding.tabLayout).attach()
+        TabLayoutMediator(binding.tabLayout, binding.viewPager) { tab, position ->
+            tab.customView = createTabView(mainPagerAdapter.getTabItem(position))
+        }.attach()
+    }
+
+    private fun createTabView(tabItem : MainPagerAdapter.Items): View{
+        return ItemMainBottomTabBinding.inflate(layoutInflater).apply {
+            this.tabItem = tabItem
+        }.root
     }
 
     companion object {
